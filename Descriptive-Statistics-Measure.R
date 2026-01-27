523bc0a03138fd61fa69c2f6a636dbdabddaa6d0
MaliqueSimpson <malsimpson@claflin.edu>
2026-01-27 10:28:17 -0500

    Add files via upload

---
+++ Descriptive-Statistics-Measure.R
@@ -0,0 +1,15 @@
+#Author: Malique; Date: 01/27/2026; Purpose: Test measures of descriptive variables
+
+# How to apply mean funtion in R
+variable1<-c(1,2,3,4,5,6,7,8,9,10)
+mean(variable1)
+#[1] 5.5
+
+
+
+#Examples
+# To check contents of container: Name it
+variable<-c(1,2,3,4,5)
+
+# To get get many values (1-1000)
+variable3<-c(1:1000)
