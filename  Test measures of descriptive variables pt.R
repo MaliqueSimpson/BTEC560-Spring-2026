3079f0ebebbb7f7eb76ae84c0b8ce7b9b28fd40e
MaliqueSimpson <malsimpson@claflin.edu>
2026-02-02 13:17:55 -0500

    Add files via upload

---
+++  Test measures of descriptive variables pt.R
@@ -0,0 +1,17 @@
+# How to apply mean function in R
+ 
+variable1 &lt;- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+mean(variable1)
+
+# [1] 5.5
+
+# How to apply a median function in R
+median(variable1)
+
+# [1] 5.5
+
+# How to apply standard deviation in R
+sd(variable1)
+
+# [1] 3.02765
+
