925641dfa25b370860052bcd961cb688443ee01d
MaliqueSimpson <malsimpson@claflin.edu>
2026-02-26 10:09:31 -0500

    Add files via upload

---
+++ Correlation-Test_Pt.2.R
@@ -0,0 +1,21 @@
+#Load library ggpubr
+library("ggpubr")
+
+#Load some dummy dataset
+my_data <- mtcars
+my_data
+
+#Generate a correlation coefficient value for variables "mpg" and "wt"
+res<-cor.test(my_data$wt, my_data$mpg, method="pearson")
+res
+
+	Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
