40abbe6db435c54d8c153a40716c9d57cadb7133
MaliqueSimpson <malsimpson@claflin.edu>
2026-04-23 09:52:55 -0400

    Add files via upload

---
+++ Chi-Square.R
@@ -0,0 +1,15 @@
+#Name: Malique Simpson, Date: 04/23/2026, Purpose: Chi-Square analysis
+
+#Read dummy dataset
+data_frame <- read.csv("https://goo.gl/j6lRXD") 
+
+#print the read dataset
+table(data_frame$treatment, data_frame$improvement) 
+
+#Apply Chi-Square function
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+
+	Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
